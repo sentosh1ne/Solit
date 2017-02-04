c9b7460f5d9f2a36c99cf8c157ffcdd4e14f21de
sentosh1ne <fortrust2@gmail.com>
2017-02-04 14:44:20 +0200

    Card pojo

--- app/src/main/java/com/sentosh1ne/sollitaire/pojos/Card.kt
+++ app/src/main/java/com/sentosh1ne/sollitaire/pojos/Card.kt
@@ -10,7 +10,7 @@ data class Card(val value: Int,val suit: String,var faceUp: Boolean){
             "${cardsMap[value]}".padEnd(2) + "${getSuitChar(suit)}"
         else
             "xxx"
-    
+
 
     companion object{
         val clubs = "Clubs"
@@ -22,13 +22,12 @@ data class Card(val value: Int,val suit: String,var faceUp: Boolean){
         val cardsMap = mapOf(0 to "A", 1 to "2", 2 to "3" , 3 to "4", 4 to "5", 5 to "6", 6 to "7", 7 to "8", 8 to "9", 9 to "10", 10 to "J", 11 to "Q", 12 to "K")
     }
 
-    private fun getSuitChar(suit: String): String {
-        when (suit) {
+    private fun getSuitChar(suit: String): String = when (suit) {
             diamonds -> "\u2666"
             clubs -> "\u2663"
             hearts -> "\u2665"
             spades -> "\u2660"
             else -> throw IllegalArgumentException("Wrong argument for $suit parameter");
         }
-    }
+
 }
